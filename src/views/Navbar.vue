1d4f0df282fcf4d527f97dc7d0a3a1e97d48c2cd
Rananjaya <rananjaya@vizuamatix,com>
2021-05-04 14:29:05 +0530

    addnavbar and slide

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -8,9 +8,41 @@
             </v-layout>
             <v-list>
                  <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
-
+                     <v-list-item-action>
+                         <v-icon>{{link.icon}}</v-icon>
+                     </v-list-item-action>
+                     <v-list-item-content>
+                         <v-list-item-title>{{link.text}}</v-list-item-title>
+                     </v-list-item-content>
                  </v-list-item>
             </v-list>
+            <v-flex class="ma-2">
+                <template>
+                    <v-carousel
+                    cycle
+                    dark
+                    height="230"
+                    hode-delimiter-backgroud
+                    show-arrow-on-hover
+                    >
+                    <v-carousel-item v-for="(slide, i) in slides" :key="i">
+                        <v-sheet
+                        color="white"
+                        height="80%"
+                        align="center"
+                        title>
+                          <v-row class="fill-height" align="center" justify="center">
+                             <div class="grey--text">
+                               <h4>Traffic fines</h4>
+                               <p class=ma-2>{{slide}}</p>
+                             </div>
+                          </v-row>
+                        </v-sheet>
+                    </v-carousel-item>
+
+                    </v-carousel>
+                </template>
+            </v-flex>
         </v-navigation-drawer>
     </nav>
 </template>
@@ -27,7 +59,19 @@ export default {
          {icon: 'fas fa-chart-line', text:'Investment', route: '/investments'},
          {icon: 'fas fa-balance-scale-right', text:'Insurance', route: '/insurance'},
          {icon: 'fas fa-briefcase', text:'Business', route: '/busines'},
+     ],
+     slides: [
+          'Online check and payment of official debts',
+          'Online check and payment of official debts',
+          'Online check and payment of official debts',
+          'Online check and payment of official debts',
      ]
  })
 }
 </script>
+
+<style scoped>
+.border{
+    border-left: 4px solid #2c3e50
+}
+</style>
